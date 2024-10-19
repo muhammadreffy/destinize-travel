ecbfc5b51b6e483832e9365b7ee32b5317085995
Muhammad Reffy <syahnataa@gmail.com>
2024-10-19 20:03:48 +0700

    fix(bug horizontal scrolling logo partners)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-const partnersLogo = document.querySelector(".partners__logo").cloneNode(true);
+document.addEventListener("DOMContentLoaded", function () {
+  function cloneLogoForSmallScreen() {
+    if (window.matchMedia("(max-width: 640px)").matches) {
+      const partnersLogo = document
+        .querySelector(".partners__logo")
+        .cloneNode(true);
+      document
+        .querySelector(".partners__logo_container")
+        .appendChild(partnersLogo);
+    }
+  }
 
-document.querySelector(".partners__logo_container").appendChild(partnersLogo);
+  cloneLogoForSmallScreen();
+
+  window.addEventListener("resize", cloneLogoForSmallScreen);
+});
